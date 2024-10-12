784ab28d45b57eec5c09ddf631b0606194eff7ac
EugeneKovalskyi <tarantul.battlefield@gmail.com>
2024-10-12 20:30:05 +0300

    make nav links clickable

--- src/components/Header/Navigation/Item.jsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-
-export default function Item({ children }) {
-	const style = 'w-28 flex items-center justify-center transition-all duration-150 hover:bg-sky-600'
-
-	return (
-		<Link className={ style }href='#'>{ children }</Link>
-	)
-}
--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,14 +1,64 @@
-import Item from "./Item"
+import { useState } from "react"
+import clsx from "clsx"
+
+import Link from "next/link"
+
+const pages = [
+	{
+		'title': "События",
+		'path': '/events'
+	},
+	{
+		'title': "Цели",
+		'path': '/targets'
+	},
+	{
+		'title': "Знания",
+		'path': '/knowledge'
+	},
+	{
+		'title': "Мысли",
+		'path': '/thoughts'
+	},
+	{
+		'title': "Книги",
+		'path': '/books'
+	},
+	{
+		'title': "Кино",
+		'path': '/cinema'
+	},
+]
 
 export default function Navigation() {
+	const [ currentPage, setCurrentPage ] = useState('/events')
+	
 	return (
-		<div className='h-full flex text-sky-50 text-xl'>
-			<Item>События</Item>
-			<Item>Цели</Item>
-			<Item>Знания</Item>
-			<Item>Мысли</Item>
-			<Item>Книги</Item>
-			<Item>Кино</Item>
-		</div>
-	)
+    <div className='h-full flex text-sky-50 text-xl'>
+
+      { pages.map((page) => {
+				const styleLink = clsx({
+					'w-28 flex items-center justify-center transition-all duration-150 hover:bg-sky-600': true,								
+					'border-b-4 border-sky-50 bg-sky-700 hover:bg-sky-700': currentPage === page.path 
+				})
+
+        return (
+          <Link
+            className={ styleLink }
+            href='#'
+            key={ page.path }
+            onClick={ handleClick }
+            path={ page.path }
+          >
+            { page.title }
+          </Link>
+        )
+
+      }) }
+    </div>
+  )
+
+	function handleClick(e) {
+		setCurrentPage(e.target.attributes.path.value)
+	}
 }
